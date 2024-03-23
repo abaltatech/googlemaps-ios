e45f08c8afef7b7677de557237be6bd88ae4511b
dkyurtov <dkyurtov@abaltatech.com>
2024-03-23 11:49:47 +0200

    Updated binary checksums.

--- Package.swift
+++ Package.swift
@@ -24,17 +24,17 @@ let package = Package(
         .binaryTarget(
             name: "GoogleMaps",
             url: "https://github.com/abaltatech/googlemaps-ios/releases/download/\(version)/GoogleMaps.xcframework.zip",
-            checksum: "0204bb0cbef4f2eb6baa03e33b294013d44fccb6699c8af391eea355ac405d34"
+            checksum: "ddfbd7637e9529022a30818e1ffa59b209f612f1a36e6789bd0e725fe772439b"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
             url: "https://github.com/abaltatech/googlemaps-ios/releases/download/\(version)/GoogleMapsBase.xcframework.zip",
-            checksum: "bd93390559b09f1c65e3079af57a0ca0c1387445fac18aad7a5d80a8fddbe675"
+            checksum: "06c007516e08d29d39829830e47222d0ca703b5be7deba0c9f5f6011653fff11"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
             url: "https://github.com/abaltatech/googlemaps-ios/releases/download/\(version)/GoogleMapsCore.xcframework.zip",
-            checksum: "1e79a556fab06ba7a9d4a16158a39384426f1aff8bb3e140d5e967376f3386c9"
+            checksum: "d54301df8dcf24c7e2c95a0e7a27743182c0e7d3b37140edf738740b6079f9c2"
         )
     ]
 )
